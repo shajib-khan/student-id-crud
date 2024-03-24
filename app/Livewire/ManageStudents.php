75008f88126e1ef9d2aee2d2ea35bf5b7fb472ec
shajib-khan <shajibkhan946@gmail.com>
2024-03-24 15:49:28 +0600

    start the project

---
+++ app/Livewire/ManageStudents.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ManageStudents extends Component
+{
+    public function render()
+    {
+        return view('livewire.manage-students');
+    }
+}
